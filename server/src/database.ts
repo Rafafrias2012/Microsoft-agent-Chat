6297d365e60e1e399c73dc5f94d242ba37a48527
Elijah R <elijah@elijahr.dev>
2024-08-14 00:50:22 -0400

    ban ipv6 by /64 range

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,5 +1,7 @@
+import { isIP } from 'net';
 import { MySQLConfig } from './config.js';
 import * as mysql from 'mysql2/promise';
+import { Address6 } from 'ip-address';
 
 export class Database {
 	private config: MySQLConfig;
@@ -24,15 +26,46 @@ export class Database {
 	}
 
 	async banUser(ip: string, username: string) {
+		let _ip = this.formatIP(ip);
 		let conn = await this.db.getConnection();
-		await conn.execute('INSERT INTO bans (ip, username) VALUES (?, ?)', [ip, username]);
+		await conn.execute('INSERT INTO bans (ip, username) VALUES (?, ?)', [_ip, username]);
 		conn.release();
 	}
 
+	private formatIP(ip: string) {
+		switch (isIP(ip)) {
+			case 4:
+				// If IPv4, just return as-is
+				return ip;
+			case 6: {
+				// If IPv6, return the /64 equivalent
+				let addr = new Address6(ip);
+				addr.subnetMask = 64;
+				return addr.startAddress().canonicalForm() + '/64';
+			}
+			case 0:
+			default:
+				// Invalid IP
+				throw new Error('Invalid IP address (what the hell did you even do???)');
+		}
+	}
+
 	async isUserBanned(ip: string): Promise<boolean> {
+		let _ip = this.formatIP(ip);
 		let conn = await this.db.getConnection();
-		let res = (await conn.query('SELECT COUNT(ip) AS cnt FROM bans WHERE ip = ?', [ip])) as mysql.RowDataPacket;
+
+		let isBanned = false;
+
+		let res = (await conn.query('SELECT COUNT(ip) AS cnt FROM bans WHERE ip = ?', [_ip])) as mysql.RowDataPacket;
+		isBanned = res[0][0]['cnt'] !== 0;
+
+		// compat with old schema
+		if (!isBanned && _ip !== ip) {
+			let res = (await conn.query('SELECT COUNT(ip) AS cnt FROM bans WHERE ip = ?', [ip])) as mysql.RowDataPacket;
+			isBanned = res[0][0]['cnt'] !== 0;
+		}
+
 		conn.release();
-		return res[0][0]['cnt'] !== 0;
+		return isBanned;
 	}
 }
