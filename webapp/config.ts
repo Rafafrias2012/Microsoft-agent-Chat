ac9e8f7f76fb37520822966c2a5e83f41a726c82
ItzRafaelScout <155685529+Rafafrias2012@users.noreply.github.com>
2025-01-09 12:43:59 +0400

    Delete the codebase

--- webapp/config.ts
+++
@@ -1,4 +0,0 @@
-export const Config = {
-	// The server address for the webapp to connect to. The below default is the same address the webapp is hosted at.
-	serverAddress: `https://msagent.chat`
-};
